87208402f3fabe95e455ecc206d69fa8cfcaf129
lb <2403445732@qq.com>
2024-04-17 23:36:43 +0800

    shell只能运行一次版,Segmentation fault明天找bug吧!

--- shell.c
+++ shell.c
@@ -1,51 +1,109 @@
-#include<unistd.h>
 #include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include <sys/types.h> 
+#include<sys/wait.h>
+#include<assert.h>
 #include<pwd.h>
+
+//宏定义
 #define TRUE 1
-#define NUM 100
-#define MAX_CMD_LEN 30
-struct user_info{
-	char *user_name;
-	char *user_passwd;
-	__uid_t pw_uid;
-	__gid_t pw_gid;
-	char *pw_gecos;
-	char *pw_dir;//用户目录
-	char *pw_shell;
-	char *host_name;
-	char *user_authen;//用户权限
-};
+#define MAX_HOSTNAME_LEN 100
+#define MAX_PATH_LEN 1000
+#define MAX_COMMEND_LEN 100
+
+//结构体定义
 typedef struct{
-	char *argv[MAX_CMD_LEN];
-	int arg_num;
-}cmdArgV;
+    char *commend[MAX_COMMEND_LEN];
+    char  filename[MAX_PATH_LEN];
+    int cmdnum; //参数个数
+}cmdArgv;
 
-char command[100];
 
+//函数定义
+void type_promote(){
+    struct passwd * pwd;
+    char hostname[MAX_HOSTNAME_LEN];//主机名
+    char pathname[MAX_PATH_LEN];//当前路径
 
-void type_promot()
-{
-	struct user_info user = {"user1","123456",1,1,"user1","~","/bin/bash","ubuntu","$"};
-	printf("%s@%s:%s%s",user.user_name,user.host_name,user.pw_dir,user.user_authen);
+    pwd = getpwuid(getuid());
+    getcwd(pathname,MAX_PATH_LEN);//获得当前工作目录
+    gethostname(hostname,MAX_HOSTNAME_LEN);//获得当前主机名
+    printf("%s@%s:%s#",pwd->pw_name,hostname,pathname);
+
 }
 
-cmdArgV getPasedCmd(char *command)
-{
-	cmdArgV parsedCmd;
+//命令分解
+cmdArgv* parsecommend(char *str,const char delimiters){
+    cmdArgv *commendArgv = (cmdArgv*)malloc(sizeof(cmdArgv));
+    if(commendArgv == NULL){
+        //申请内存失败
+        printf("申请内存失败\n");
+        return NULL;
+    }
+    //遍历 str
+    int row = 0,colum = 0,flag = 1,index = 0; //指向命令二维数组的行指针,列的指针
+    for(int i=0;i<strlen(str);i++){
+        //第一个分割符之前的都为命令
+        if(str[i] != delimiters){
+            commendArgv -> commend[row][colum] = str[i];
+            if(flag == 1){
+                commendArgv -> filename[colum] = str[i];
+                index = colum;
+            }
+            colum++;
+            
+        }
+        else{
+            commendArgv -> commend[row][colum] = '\0';
+            row++;
+            colum = 0;
+            flag = 0;
+            commendArgv -> cmdnum++;
+        }
+
+    }
+    commendArgv -> cmdnum = commendArgv -> cmdnum+1;//命令数等于空格数加一
+    commendArgv -> filename[++index] = '\0';
+    //命令数组最后一行加一个NULL
+    commendArgv -> commend[row+1] = NULL;
+    return commendArgv;
 
 }
+
 int main()
 {
-	while(TRUE)
-	{
-		//print a tip
-		type_promot();
-		fgets(command,100,stdin);
-		//input a commant
-		
+    while(TRUE){
+        //打印命令提示符
+        type_promote();
+        //输入命令
+        char commend[MAX_COMMEND_LEN];
+        fgets(commend,MAX_COMMEND_LEN,stdin);
+        commend[strlen(commend)-1] = '\0';
+        //分解命令
+        cmdArgv* parsecmd = parsecommend(commend,' ');
+        printf("%d",parsecmd->cmdnum);
+        for(int i=0;i<parsecmd -> cmdnum;i++){
+            for(int j=0;j<strlen(parsecmd -> commend[i]);j++){
+                printf("%c",parsecmd -> commend[i][j]);
+            }
+            printf("\n");
+        }
+        //执行命令
+        char filepath[MAX_PATH_LEN] = "/bin/";
+        strcat(filepath,parsecmd -> filename);
+        printf("%s\n",filepath);
+        int status;
+        if(fork() !=0){
+            waitpid(-1,&status,0);
+        }
+        else{
+            execve(filepath,parsecmd -> commend,NULL);
+        }
+        commend[0] = '\0';
+    }
 
 
-		//excute a commant
-	}
-	return 0;
+    return 0;
 }
